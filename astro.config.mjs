233100bdddf3e5298f574bc6b0142e63701d36bd
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2026-02-05 00:33:30 +0530

    ui tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ import remarkSmartypants from "remark-smartypants";
 export default defineConfig({
   site: "https://arv-anshul.github.io",
   base: "/v2",
+  trailingSlash: "always",
   server: {
     port: 3000,
   },
